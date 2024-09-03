1ebc0e06d002c03fcfbe960df2b2d84fff1f1e98
baxtiyorov2010 <torabaxtiyorov29@gmail.com>
2024-09-03 19:57:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let a = prompt("string typedagi malunotni kiriting")
+let b = +prompt("number typedagi malunotni kiriting")
+let d = prompt("boolen typedagi malunotni kiriting")
+confirm("ishonchingiz komilmi")
+
+if (d === "true" || d === "false" && a === String && b == number) {
+    alert(`sizritgan ${a} string tyupga tegishli \n
+    sizritgan ${b} number tyupga tegishli \n
+    sizritgan ${d} boolen tyupga tegishli \n`)
+}else{
+    confirm("qaytadan urinib koring hato bor")
+}
